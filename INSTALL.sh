451f3c386072f0de5f377bdab6c96df5a9465089
rb <rb@rbs-MacBook-Pro.local>
2021-10-28 22:03:52 +0200

    chore: linux INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
-ln -sf $HOME/workspace/dotfiles/config/sway $HOME/.config/
-ln -sf $HOME/workspace/dotfiles/config/wofi $HOME/.config/
-ln -sf $HOME/workspace/dotfiles/config/waybar $HOME/.config/
-ln -sf $HOME/workspace/dotfiles/config/alacritty $HOME/.config/
-ln -sf $HOME/workspace/dotfiles/config/redshift $HOME/.config/
-ln -sf $HOME/workspace/dotfiles/config/kanshi $HOME/.config/
-ln -sf $HOME/workspace/dotfiles/gitconfig $HOME/.gitconfig
-ln -sf $HOME/workspace/dotfiles/tmux.conf $HOME/.tmux.conf
-ln -sf $HOME/workspace/dotfiles/bash_profile $HOME/.bash_profile
-ln -sf $HOME/workspace/dotfiles/bashrc $HOME/.bashrc
-ln -sf $HOME/workspace/dotfiles/config/sublime-text-3/Packages/User/Preferences.sublime-settings $HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
-ln -sf $HOME/workspace/dotfiles/config/sublime-text-3/Packages/User/Package\ Control.sublime-settings $HOME/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings
-ln -sf $HOME/workspace/dotfiles/config/sublime-text-3/Packages/User/GitBlame.py $HOME/.config/sublime-text-3/Packages/User/GitBlame.py
-ln -sf $HOME/workspace/dotfiles/config/nvim $HOME/.config/nvim
+CWD="$(pwd)"
+
+ln -sf ${CWD}/config/sway $HOME/.config/
+ln -sf ${CWD}/config/wofi $HOME/.config/
+ln -sf ${CWD}/config/waybar $HOME/.config/
+ln -sf ${CWD}/config/alacritty $HOME/.config/
+ln -sf ${CWD}/config/redshift $HOME/.config/
+ln -sf ${CWD}/config/kanshi $HOME/.config/
+ln -sf ${CWD}/gitconfig $HOME/.gitconfig
+ln -sf ${CWD}/tmux.conf $HOME/.tmux.conf
+ln -sf ${CWD}/bash_profile $HOME/.bash_profile
+ln -sf ${CWD}/bashrc $HOME/.bashrc
+ln -sf ${CWD}/config/sublime-text-3/Packages/User/Preferences.sublime-settings $HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+ln -sf ${CWD}/config/sublime-text-3/Packages/User/Package\ Control.sublime-settings $HOME/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings
+ln -sf ${CWD}/config/sublime-text-3/Packages/User/GitBlame.py $HOME/.config/sublime-text-3/Packages/User/GitBlame.py
+ln -sf ${CWD}/config/nvim $HOME/.config/nvim
 for i in $(/bin/ls ./scripts)
 do
-  ln -sf $HOME/workspace/dotfiles/scripts/$i $HOME/bin/$i
+  ln -sf $CWD/scripts/$i $HOME/bin/$i
 done
