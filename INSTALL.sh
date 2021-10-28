f686c76d31d3105ccf584d85b5e4a43bcff6c041
Romain Bureau <rb@hio.fr>
2021-10-28 22:27:18 +0200

    chore: init macos

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,21 +2,11 @@
 
 CWD="$(pwd)"
 
-ln -sf ${CWD}/config/sway $HOME/.config/
-ln -sf ${CWD}/config/wofi $HOME/.config/
-ln -sf ${CWD}/config/waybar $HOME/.config/
-ln -sf ${CWD}/config/alacritty $HOME/.config/
-ln -sf ${CWD}/config/redshift $HOME/.config/
-ln -sf ${CWD}/config/kanshi $HOME/.config/
-ln -sf ${CWD}/gitconfig $HOME/.gitconfig
-ln -sf ${CWD}/tmux.conf $HOME/.tmux.conf
-ln -sf ${CWD}/bash_profile $HOME/.bash_profile
-ln -sf ${CWD}/bashrc $HOME/.bashrc
-ln -sf ${CWD}/config/sublime-text-3/Packages/User/Preferences.sublime-settings $HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
-ln -sf ${CWD}/config/sublime-text-3/Packages/User/Package\ Control.sublime-settings $HOME/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings
-ln -sf ${CWD}/config/sublime-text-3/Packages/User/GitBlame.py $HOME/.config/sublime-text-3/Packages/User/GitBlame.py
-ln -sf ${CWD}/config/nvim $HOME/.config/nvim
-for i in $(/bin/ls ./scripts)
-do
-  ln -sf $CWD/scripts/$i $HOME/bin/$i
-done
+ln -sf ${CWD}/gitconfig ${HOME}/.gitconfig
+ln -sf ${CWD}/tmux.conf ${HOME}/.tmux.conf
+ln -sf ${CWD}/bash_profile ${HOME}/.bash_profile
+ln -sf ${CWD}/bashrc ${HOME}/.bashrc
+ln -sf ${CWD}/config/sublime-text/Packages/User/Preferences.sublime-settings ${HOME}/Library/Application\ Support/Sublime\ Text/Packages/Preferences.sublime-settings
+ln -sf ${CWD}/config/sublime-text/Packages/User/Package\ Control.sublime-settings ${HOME}/Library/Application\ Support/Sublime\ Text/Packages/Package\ Control.sublime-settings
+ln -sf ${CWD}/config/sublime-text/Packages/User/GitBlame.py ${HOME}/Library/Application\ Support/Sublime\ Text/Packages/User/GitBlame.py
+ln -sf ${CWD}/config/nvim ${HOME}/.config/
--- bashrc
+++ bashrc
@@ -4,16 +4,12 @@
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
 
-unset DISPLAY # fix firefox (use wayland instead of x11)
-
 export GPG_TTY=$(tty)
 export HISTSIZE=10000
 export HISTCONTROL=ignorespace:erasedups
 export HISTIGNORE="ls:ps:history"
 
 [[ -f ~/.localrc ]] && . ~/.localrc
-[[ -f /usr/share/fzf/completion.bash ]] && . /usr/share/fzf/completion.bash
-[[ -f /usr/share/fzf/key-bindings.bash ]] && . /usr/share/fzf/key-bindings.bash
 
 dotfiles_dir="$(dirname $(readlink $HOME/.bashrc))"
 if [[ -d "$dotfiles_dir/functions" ]]
