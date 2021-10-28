2577c8318550c6d9e34cb51f63aab614cd2a114c
Romain Bureau <rb@hio.fr>
2021-10-28 22:38:53 +0200

    chore: macos adaptations

--- INSTALL.sh
+++ INSTALL.sh
@@ -6,7 +6,7 @@ ln -sf ${CWD}/gitconfig ${HOME}/.gitconfig
 ln -sf ${CWD}/tmux.conf ${HOME}/.tmux.conf
 ln -sf ${CWD}/bash_profile ${HOME}/.bash_profile
 ln -sf ${CWD}/bashrc ${HOME}/.bashrc
-ln -sf ${CWD}/config/sublime-text/Packages/User/Preferences.sublime-settings ${HOME}/Library/Application\ Support/Sublime\ Text/Packages/Preferences.sublime-settings
-ln -sf ${CWD}/config/sublime-text/Packages/User/Package\ Control.sublime-settings ${HOME}/Library/Application\ Support/Sublime\ Text/Packages/Package\ Control.sublime-settings
+ln -sf ${CWD}/config/sublime-text/Packages/User/Preferences.sublime-settings ${HOME}/Library/Application\ Support/Sublime\ Text/Packages/User/Preferences.sublime-settings
+ln -sf ${CWD}/config/sublime-text/Packages/User/Package\ Control.sublime-settings ${HOME}/Library/Application\ Support/Sublime\ Text/Packages/User/Package\ Control.sublime-settings
 ln -sf ${CWD}/config/sublime-text/Packages/User/GitBlame.py ${HOME}/Library/Application\ Support/Sublime\ Text/Packages/User/GitBlame.py
 ln -sf ${CWD}/config/nvim ${HOME}/.config/
