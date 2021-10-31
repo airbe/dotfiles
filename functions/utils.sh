15a489c68f81da3d9246916bb8799e7e447ee83d
Romain Bureau <rb@hio.fr>
2021-10-31 22:53:30 +0100

    chore: cleanup

--- functions/utils.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-function screenshot() {
-  local date="$(date +'%Y-%m-%d-%H%M%S')"
-  local dir="$HOME/Screenshots/"
-  mkdir -p "$dir"
-  grim -g "$(slurp)" "$dir/$date.png"
-}
