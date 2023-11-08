5acb07391f41ef779edb2f75f1461f98d8bbcb3f
Romain Bureau <rb@hio.fr>
2023-11-08 21:21:43 +0100

    chore(): hyprland + waybar upgrade

--- functions/ntfy.sh
+++ functions/ntfy.sh
@@ -4,5 +4,14 @@
 ntfy ()
 {
   local MESSAGE="${1}"
-  curl -u "${NTFY_CREDENTIAL}" -d "${MESSAGE}" ${NTFY_URL}/${NTFY_TOPIC}
+  curl -u "${NTFY_CREDENTIAL}" -d "${MESSAGE}" "${NTFY_URL}/${NTFY_TOPIC}"
+}
+
+ntfya ()
+{
+  local MESSAGE="${1}"
+  local ATTACHMENT="${2}"
+  local FILENAME="$(basename ${ATTACHMENT})"
+
+  echo curl -u "${NTFY_CREDENTIAL}" -T "${ATTACHMENT}" -H "Filename: ${FILENAME}" -d "${MESSAGE}" "${NTFY_URL}/${NTFY_TOPIC}"
 }
